4da1140391ea55a6f62d1faa4b2c53fab67dd1d9
David Milum <david.milum.89@gmail.com>
2014-04-25 12:16:33 +0000

    missing the deadline. I found out perl is not going to support a colored terminal, so Im gonna try python

--- a.pl
+++ a.pl
@@ -1,6 +1,18 @@
 use Term::ReadLine;
+
+chomp($tty = `tty`);
+$ENV{"TTY"}=$tty;
+chomp($id = `ktty register`);
+$ENV{"ID"}=$id;
+chomp($record_str = `ktty get-all`);
+@record_arr = split(",",$record_str);
+($id, $locked, $tty, $log_file, $tty_cd, $pid, $label, $command_segment) = @record_arr;
+
+$green="\e[1;32m";
+#print `tput cuf 1`;
+$reset="\033[0m";
 $term = new Term::ReadLine 'ProgramName';
-$term->readline('prompt>', 'starting value');
+$term->readline("$green$tty$reset \[$id\] ktty>  ",$command_segment);
 #$f = $term->Features;
 #%derefd = %$f;
 #for my $key (sort keys %derefd) {
